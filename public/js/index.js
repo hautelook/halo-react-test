f0f685f6c09181534ac6f7a6d38ab567a2ec108e
Herman J. Radtke III <herman@hermanradtke.com>
2015-10-12 08:35:16 -0700

    Update to react 0.14

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,15 @@
-var Index = React.createClass({
-  render: function() {
-    return (
-      <article>
-        <h1>HauteLook React Test</h1>
-        <p>You will be replacing this page with a grid of HauteLook flash events.</p>
-        <ul>
-          <li>The json data of events is at <a href="/events">/events</a></li>
-        </ul>
-        <p>Not sure where to start? Check out the README!</p>
-      </article>
-    )
-  }
-});
+var Index = function(props) {
+  return (
+    <article>
+      <h1>HauteLook React Test</h1>
+      <p>You will be replacing this page with a grid of HauteLook flash events.</p>
+      <ul>
+        <li>The json data of events is at <a href="/events">/events</a></li>
+      </ul>
+      <p>Not sure where to start? Check out the README!</p>
+    </article>
+  );
+}
 
 React.render(
   <Index />,
